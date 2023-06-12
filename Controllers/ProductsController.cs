bc74122edc027e784c89edf5e7b606ff1d071715
Licínio Rosário <83244862+ldvssr@users.noreply.github.com>
2023-06-13 00:18:33 +0100

    Register new users

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -82,9 +82,8 @@ namespace SuperShop.Controllers
                 }
 
                 var product = _converterHelper.ToProduct(model, imageId, true);
-
-                //TODO: MODIFICAR PARA O USER QUE ESTIVER LOGADO
-                product.User = await _userHelper.GetUserByEmailAsync("ldvssr@icloud.com");
+                               
+                product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                 await _productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
@@ -129,9 +128,8 @@ namespace SuperShop.Controllers
                     }
 
                     var product = _converterHelper.ToProduct(model, imageId, false);
-
-                    //TODO: MODIFICAR PARA O USER QUE ESTIVER LOGADO
-                    product.User = await _userHelper.GetUserByEmailAsync("ldvssr@icloud.com");
+                                        
+                    product.User = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
                     await _productRepository.UpdateAsync(product);
 
                 }
@@ -152,6 +150,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
