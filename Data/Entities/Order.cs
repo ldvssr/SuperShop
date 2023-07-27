bfa4ed73f6402d4851fca6a96ba8f38366bb8255
Licínio Rosário <83244862+ldvssr@users.noreply.github.com>
2023-07-27 19:40:04 +0100

    visual details

--- Data/Entities/Order.cs
+++ Data/Entities/Order.cs
@@ -14,11 +14,10 @@ namespace SuperShop.Data.Entities
         [Display(Name = "Order date")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime OrderDate { get; set; }
-
-        [Required]
+                
         [Display(Name = "Delivery date")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
-        public DateTime DeliveryDate { get; set; }
+        public DateTime? DeliveryDate { get; set; }
 
         [Required]
         public User User { get; set; }
